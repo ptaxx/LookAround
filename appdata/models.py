fa1c18ef1422c1300f7546ada1f737cd21b5b037
taneltarkvee <tanel.tarkvee@gmail.com>
2024-11-17 00:35:36 +0100

    area model by choice. Right now we have one location

--- appdata/models.py
+++ appdata/models.py
@@ -21,6 +21,16 @@ class Team(models.Model):
     def __str__(self):
         return self.name
 
+area_choices = ("Trysil")
+
+class Area(models.Model):
+    area_name = models.CharField(max_length=20, choices=area_choices, default="Trysil")
+
+    def __str__(self):
+        return self.area_name
+
+
+
 
 class Activity(models.Model):
     short_description = models.TextField(max_length=140)
