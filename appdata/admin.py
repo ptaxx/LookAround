995dfd644a8e737348aa1e777ed20273b2664dec
Daniil Popov <daniil.popov@gmail.com>
2024-11-17 08:34:39 +0200

    Add model BusinessClient, fix raleations, add Admin Page, make migrations

--- appdata/admin.py
+++ appdata/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import User, BusinessClient, Team, Area, Venue, Activity, Game
 
-# Register your models here.
+
+admin.site.register(User)
+
+admin.site.register(BusinessClient)
+
+admin.site.register(Team)
+
+admin.site.register(Area)
+
+admin.site.register(Venue)
+
+admin.site.register(Activity)
+
+admin.site.register(Game)
+
+
+
+
--- appdata/models.py
+++ appdata/models.py
@@ -9,7 +9,15 @@ class User(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(null=True, blank=True)
     short_bio = models.TextField(max_length=200, blank=True)
-    user_pic = models.ImageField(upload_to='userpics', null=True)
+    
+    def __str__(self):
+        return self.username
+    
+
+class BusinessClient(models.Model):
+    username = models.CharField(max_length=32)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(null=True, blank=True)
 
     def __str__(self):
         return self.username
@@ -17,29 +25,33 @@ class User(models.Model):
 
 class Team(models.Model):
     name = models.CharField(max_length=32)
-    moderator = models.ForeignKey(User, on_delete=models.CASCADE)
-    members = models.ManyToManyField(User, null=True, on_delete=models.SET_NULL)
+    moderator = models.ForeignKey(User, related_name='moderator', on_delete=models.CASCADE)
+    member = models.ManyToManyField(User, related_name='member')
 
     def __str__(self):
         return self.name
+    
 
-area_choices = ("Trysil")
+class TeamUser(models.Model):
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+
+
+
 
 class Area(models.Model):
-    area_name = models.CharField(max_length=20, choices=area_choices, default="Trysil")
+    area_name = models.CharField(max_length=32)
 
     def __str__(self):
         return self.area_name
 
 
-
-
 class Venue(models.Model):
     venue_name = models.CharField(max_length=32)
-    area = models.ForeignKey(Area)
-    venue_work_hours = models.IntegerField(from_hour = models.TimeField(),to_hour = models.TimeField(), blank=False)
+    area = models.ForeignKey(Area, on_delete=models.CASCADE)
+    venue_opening_hour = models.TimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
+    venue_closing_hour = models.TimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
     description_of_the_venue = models.TextField(max_length=200, blank=False)
-    venue_logo = models.ImageField(upload_to='venuelogo/', blank=False)
     venue_link_tripadvisor = models.URLField(max_length=200, blank=False)
 
     def __str__(self):
@@ -60,15 +72,25 @@ class Activity(models.Model):
 class Game(models.Model):
     starting_time = models.TimeField(auto_now=False, auto_now_add=False)
     finishing_time = models.TimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
-    GAME_SIZE_CHOICES = (
-        ('9', 9)
-        ('16', 16)
-    )
+    GAME_SIZE_CHOICES = [
+        ('9', 9),
+        ('16', 16),
+    ]
     game_size = forms.MultipleChoiceField(choices=GAME_SIZE_CHOICES)
     availability = models.BooleanField(default=True)
-    area = models.ForeignKey(Area)
-    players = models.ManyToManyField(User, Null=True, on_delete=models.SET_NULL)
-    activities = models.ManyToManyField(Activity, on_delete=models.CASCADE)
+    area = models.ForeignKey(Area, on_delete=models.CASCADE)
+    players = models.ManyToManyField(User)
+    activities = models.ManyToManyField(Activity)
 
     def __str__(self):
         return self.starting_time
+    
+
+class GameActivity(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    activity = models.ForeignKey(Activity, on_delete=models.CASCADE)
+
+
+class GameUser(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
