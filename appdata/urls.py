a229765ddfc056f3fc752b57c70e477cc1834765
Daniil Popov <daniil.popov@gmail.com>
2024-11-20 15:46:15 +0200

    Add login page

--- appdata/urls.py
+++ appdata/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-
+from django.contrib.auth.views import LoginView, LogoutView
 from appdata.views import ActivityView, GamePageView, GamesPageView
 
 urlpatterns = [
     # path('', IndexView.as_view(), name='index'),
     path('gamespage/', GamesPageView.as_view(), name='gamespage'),
     path('gamepage/<int:pk>/', GamePageView.as_view(), name='gamepage'),
-    path('activitypage/<int:pk>/', ActivityView.as_view(), name='activitypage')
+    path('activitypage/<int:pk>/', ActivityView.as_view(), name='activitypage'),
+    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
