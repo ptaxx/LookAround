fd4e4cf755911623691c4ddde2da9d3f8ce364f9
Daniil Popov <daniil.popov@gmail.com>
2024-11-21 16:21:16 +0200

    Add "userpic" to registration form

--- appdata/forms.py
+++ appdata/forms.py
@@ -4,5 +4,5 @@ from django.contrib.auth.forms import UserCreationForm
 class SignUpForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = ['isplayer', 'username', 'first_name', 'last_name', 'email', 'short_bio']
+        fields = ['isplayer', 'username', 'first_name', 'last_name', 'email', 'short_bio', 'userpic']
         labels = {'isplayer': 'Register as player', 'email': 'Email'}
