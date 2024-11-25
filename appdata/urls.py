0f8205eaf2b0f65702a3502a7f64a9186d4db978
taneltarkvee <tanel.tarkvee@gmail.com>
2024-11-25 20:10:04 +0100

    double url

--- appdata/urls.py
+++ appdata/urls.py
@@ -26,8 +26,7 @@ urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('userpage/<int:pk>/', UserPageViews.as_view(), name='userpage'),
     path('createactivity', ActivityCreationFormView.as_view(), name='createactivity'),
-    path('creategame', GameEntryView.as_view(), name='index'),
+    path('creategame', GameEntryView.as_view(), name='creategame'),
     path('contactpage/', ContactPage.as_view(), name='contact'),
-    path('createactivity', ActivityCreationFormView.as_view(), name='createactivity')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
