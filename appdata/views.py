fadfadeb140f65dde9266070a137e08e489ae1f7
Daniil Popov <daniil.popov@gmail.com>
2024-11-30 23:08:01 +0200

    Format views.py

--- appdata/views.py
+++ appdata/views.py
@@ -5,22 +5,22 @@ from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse, reverse_lazy
 from django.views import View
 from .forms import (
-    SignUpForm, 
-    GameCreationForm, 
+    SignUpForm,
+    GameCreationForm,
     ActivityCreationForm,
     VenueCreationForm,
     TeamCreationForm,
     PasscodeForm,
-    )
+)
 from django.contrib import messages
 from appdata.models import (
-    Activity, 
-    Game, 
-    Team, 
-    Area, 
-    CustomUser, 
-    Venue, 
-    ActivityCheck, 
+    Activity,
+    Game,
+    Team,
+    Area,
+    CustomUser,
+    Venue,
+    ActivityCheck,
     ScoreBoard,
 )
 from django.template import loader
@@ -32,95 +32,88 @@ from django.utils import timezone
 
 class GamePageView(View):
     def get(self, request, *args, **kwargs):
-        game = Game.objects.get(id=kwargs.get('pk'))
+        game = Game.objects.get(id=kwargs.get("pk"))
         activities = game.activities.all()
         players = game.players.all()
-        scoreboards = ScoreBoard.objects.filter(game=game).order_by('-points')
+        scoreboards = ScoreBoard.objects.filter(game=game).order_by("-points")
         teams = Team.objects.filter(team_user__in=game.players.all()).distinct()
         area_id = game.area.weather_id
         weather_data = get_weather_data(area_id)
         time_data = countdown_timer(game.starting_time)
         context = {
-            'game': game, 
-            'activities': activities, 
-            'players': players, 
-            'teams':teams,
-            'weather_data': weather_data,
-            'scoreboards': scoreboards,
-            'time_data': time_data,
+            "game": game,
+            "activities": activities,
+            "players": players,
+            "teams": teams,
+            "weather_data": weather_data,
+            "scoreboards": scoreboards,
+            "time_data": time_data,
         }
-        return render(request, 'gamepage.html', context)
-    
+        return render(request, "gamepage.html", context)
+
+
 class AreaPageView(View):
     def get(self, request, *args, **kwargs):
-        area = Area.objects.get(id=kwargs.get('pk'))
+        area = Area.objects.get(id=kwargs.get("pk"))
         venues = Venue.objects.filter(area=area)
         area_id = area.weather_id
         weather_data = get_weather_data(area_id)
-        context = {
-            'area': area, 
-            'weather_data': weather_data,
-            'venues': venues
-            }
-        return render(request, 'areapage.html', context)
+        context = {"area": area, "weather_data": weather_data, "venues": venues}
+        return render(request, "areapage.html", context)
+
     def post(self, request, *args, **kwargs):
-        area = Area.objects.get(id=kwargs.get('pk'))
+        area = Area.objects.get(id=kwargs.get("pk"))
         venues = Venue.objects.filter(area=area)
         area_id = area.weather_id
         weather_data = get_weather_data(area_id)
-        context = {
-            'area': area, 
-            'weather_data': weather_data,
-            'venues': venues
-            }
-        if 'set_area' in request.POST:
-            area = Area.objects.get(id=kwargs.get('pk'))
+        context = {"area": area, "weather_data": weather_data, "venues": venues}
+        if "set_area" in request.POST:
+            area = Area.objects.get(id=kwargs.get("pk"))
             profile = request.user
             profile.current_area = area
             profile.save()
-            return render(request, 'areapage.html', context)
-    
+            return render(request, "areapage.html", context)
+
 
 class VenuePageView(View):
     def get(self, request, *args, **kwargs):
-        venue = Venue.objects.get(id=kwargs.get('pk'))
-        context = {'venue': venue}
-        return render(request, 'venuepage.html', context)
-    
-    
+        venue = Venue.objects.get(id=kwargs.get("pk"))
+        context = {"venue": venue}
+        return render(request, "venuepage.html", context)
+
+
 class ActivityView(View):
     def get(self, request, *args, **kwargs):
-        activity = Activity.objects.get(id=kwargs.get('pk'))
+        activity = Activity.objects.get(id=kwargs.get("pk"))
         form = PasscodeForm()
         user = request.user
         user_has_activity = user_activity_check(user, activity)
         context = {
-            'activity': activity, 
-            'form': form,
-            'user_has_activity': user_has_activity,
-            }
-        return render(request, 'activitypage.html', context)
+            "activity": activity,
+            "form": form,
+            "user_has_activity": user_has_activity,
+        }
+        return render(request, "activitypage.html", context)
+
     def post(self, request, *args, **kwargs):
-        activity = Activity.objects.get(id=kwargs.get('pk'))
+        activity = Activity.objects.get(id=kwargs.get("pk"))
         user = request.user
         user_has_activity = user_activity_check(user, activity)
         form = PasscodeForm(request.POST)
         if form.is_valid():
-            passcode = form.cleaned_data['passcode']
+            passcode = form.cleaned_data["passcode"]
             if passcode == activity.passcode:
-                messages.success(request, 'The passcode is correct! Task complete!')
+                messages.success(request, "The passcode is correct! Task complete!")
                 activity_checks = ActivityCheck.objects.filter(
-                    activity=activity, 
-                    user=request.user
-                    )
+                    activity=activity, user=request.user
+                )
                 for entry in activity_checks:
                     entry.is_active = False
                     entry.save()
                 games = Game.objects.filter(activities=activity)
                 for game in games:
                     scoreboards = ScoreBoard.objects.filter(
-                        game=game,
-                        user=request.user
+                        game=game, user=request.user
                     )
                     for scoreboard in scoreboards:
                         if scoreboard.points < 9:
@@ -129,48 +122,49 @@ class ActivityView(View):
                             if scoreboard.points == 9:
                                 scoreboards = ScoreBoard.objects.filter(game=game)
                                 positions = [entry.position for entry in scoreboards]
-                                if '3rd place' in positions:
-                                    scoreboard.position = 'Finished'
+                                if "3rd place" in positions:
+                                    scoreboard.position = "Finished"
                                     scoreboard.save()
-                                elif '2nd place' in positions:
-                                    scoreboard.position = '3rd place'
+                                elif "2nd place" in positions:
+                                    scoreboard.position = "3rd place"
                                     scoreboard.save()
-                                elif '1st place' in positions:
-                                    scoreboard.position = '2nd place'
+                                elif "1st place" in positions:
+                                    scoreboard.position = "2nd place"
                                     scoreboard.save()
                                 else:
-                                    scoreboard.position = '1st place'
+                                    scoreboard.position = "1st place"
                                     scoreboard.save()
                 return redirect(request.path)
             else:
-                messages.error(request, 'Incorrect passcode. Please try again.')
+                messages.error(request, "Incorrect passcode. Please try again.")
         context = {
-            'activity': activity, 
-            'form': form,
-            'user_has_activity': user_has_activity,
-            }
-        return render(request, 'activitypage.html', context)
-    
+            "activity": activity,
+            "form": form,
+            "user_has_activity": user_has_activity,
+        }
+        return render(request, "activitypage.html", context)
+
 
 class AreasPageView(View):
     def get(self, request, *args, **kwargs):
         areas = Area.objects.all()
-        context = {'areas': areas}
-        return render(request, 'areaspage.html', context)
+        context = {"areas": areas}
+        return render(request, "areaspage.html", context)
 
 
 class TeamsPageView(View):
     def get(self, request, *args, **kwargs):
         teams = Team.objects.all()
-        context = {'teams': teams}
-        return render(request, 'teamspage.html', context)
+        context = {"teams": teams}
+        return render(request, "teamspage.html", context)
+
 
 class GamesPageView(View):
     def get(self, request, *args, **kwargs):
         games = Game.objects.all()
-        context = {'games': games}
-        return render(request, 'gamespage.html', context)
-    
+        context = {"games": games}
+        return render(request, "gamespage.html", context)
+
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
@@ -179,66 +173,70 @@ class IndexView(View):
         user = request.user
         if user.is_authenticated and user.current_area:
             area_id = user.current_area.weather_id
-        else: 
+        else:
             area_id = Area.objects.first().weather_id
         weather_data = get_weather_data(area_id)
-        context = {'games': games, 'area': area, 'weather_data': weather_data}
+        context = {"games": games, "area": area, "weather_data": weather_data}
         return render(request, "index.html", context)
 
-      
+
 class SignUpView(CreateView):
     form_class = SignUpForm
-    template_name = 'registration/signup.html'
-    success_url = reverse_lazy('login')  # Redirects to login if registration was successful
-    
+    template_name = "registration/signup.html"
+    success_url = reverse_lazy(
+        "login"
+    )  # Redirects to login if registration was successful
+
     def form_valid(self, form):
-        messages.success(self.request, 'Registration was successful!')
+        messages.success(self.request, "Registration was successful!")
         response = super().form_valid(form)
         return response
-    
+
 
 class UserPageViews(View):
     def get(self, request, *args, **kwargs):
-        user = CustomUser.objects.get(id=kwargs.get('pk'))
+        user = CustomUser.objects.get(id=kwargs.get("pk"))
         # Find all teams where at least one player is part of
         teams = Team.objects.filter(
-            team_user__in=user.game_set.values_list('players', flat=True)
-            ).distinct()
+            team_user__in=user.game_set.values_list("players", flat=True)
+        ).distinct()
         # Find all games where user is the player
         games = Game.objects.filter(players=user).distinct()
-        context = {'user': user, 'teams': teams, 'games': games}
-        return render(request, 'userpage.html', context)
+        context = {"user": user, "teams": teams, "games": games}
+        return render(request, "userpage.html", context)
 
-    
+
 class TeamPageViews(View):
     def get(self, request, *args, **kwargs):
-        team = Team.objects.get(id=kwargs.get('pk'))
+        team = Team.objects.get(id=kwargs.get("pk"))
         games = Game.objects.filter(teams=team)
         teamplayers = team.team_user.all()
-        context = {'team': team, 
-                   'teamplayers': teamplayers,
-                   'games': games,
-                   }
-        return render(request, 'teampage.html', context)
+        context = {
+            "team": team,
+            "teamplayers": teamplayers,
+            "games": games,
+        }
+        return render(request, "teampage.html", context)
 
 
 class GameEntryView(FormView):
     form_class = GameCreationForm
-    template_name = 'creategame.html'
-    success_url = '/'
+    template_name = "creategame.html"
+    success_url = "/"
+
     def form_valid(self, form):
         self.game = Game.objects.create(
-            area=form.cleaned_data.get('area'),
-            starting_time=form.cleaned_data.get('starting_time'),
-            finishing_time=form.cleaned_data.get('finishing_time'),
-            availability=form.cleaned_data.get('availability'),
-            )
-        for player in form.cleaned_data['players']:
+            area=form.cleaned_data.get("area"),
+            starting_time=form.cleaned_data.get("starting_time"),
+            finishing_time=form.cleaned_data.get("finishing_time"),
+            availability=form.cleaned_data.get("availability"),
+        )
+        for player in form.cleaned_data["players"]:
             self.game.players.add(player)
-        for team in form.cleaned_data['teams']:
-                self.game.teams.add(team)
-        
-        venues = Venue.objects.filter(area=form.cleaned_data.get('area'))
+        for team in form.cleaned_data["teams"]:
+            self.game.teams.add(team)
+
+        venues = Venue.objects.filter(area=form.cleaned_data.get("area"))
         if len(venues) < 9:
             for x in venues:
                 activities = Activity.objects.filter(venue=x)
@@ -253,7 +251,7 @@ class GameEntryView(FormView):
                 if activities.exists():
                     activity = choice(activities)
                     self.game.activities.add(activity)
-                    
+
         activities = self.game.activities.all()
         players = set(self.game.players.all())
         teams = self.game.teams.all()
@@ -272,68 +270,75 @@ class GameEntryView(FormView):
             ScoreBoard.objects.create(
                 game=self.game,
                 user=player,
-            )  
-                           
+            )
+
         return super(GameEntryView, self).form_valid(form)
+
     def get_success_url(self):
-        return reverse('gamepage', kwargs={'pk': self.game.pk})
-    
-    
+        return reverse("gamepage", kwargs={"pk": self.game.pk})
+
+
 class ContactPage(View):
     def get(self, request):
-        return render(request, 'contactpage.html')
+        return render(request, "contactpage.html")
 
 
 class ActivityCreationFormView(FormView):
-    template_name = 'createactivity.html'
+    template_name = "createactivity.html"
     form_class = ActivityCreationForm
-    success_url = '/'
+    success_url = "/"
+
     def form_valid(self, form):
         self.activity = Activity.objects.create(
-            short_description=form.cleaned_data.get('short_description'),
-            full_description=form.cleaned_data.get('full_description'),
-            venue=form.cleaned_data.get('venue'),
-            passcode=str(randint(10000,99999))
-            )
+            short_description=form.cleaned_data.get("short_description"),
+            full_description=form.cleaned_data.get("full_description"),
+            venue=form.cleaned_data.get("venue"),
+            passcode=str(randint(10000, 99999)),
+        )
         return super(ActivityCreationFormView, self).form_valid(form)
+
     def get_success_url(self):
-        return reverse('activitypage', kwargs={'pk': self.activity.pk})
-    
-    
+        return reverse("activitypage", kwargs={"pk": self.activity.pk})
+
+
 class VenueCreationFormView(FormView):
-    template_name = 'createvenue.html'
+    template_name = "createvenue.html"
     form_class = VenueCreationForm
-    success_url = '/'
+    success_url = "/"
+
     def form_valid(self, form):
         self.venue = Venue.objects.create(
-            name=form.cleaned_data.get('name'),
-            area=form.cleaned_data.get('area'),
-            opening_hour=form.cleaned_data.get('opening_hour'),
-            closing_hour=form.cleaned_data.get('closing_hour'),
-            description=form.cleaned_data.get('description'),
-            contact=form.cleaned_data.get('contact'),
-            picture=form.cleaned_data.get('picture'),
-            tripadvisor_link=form.cleaned_data.get('tripadvisor_link'),
-            )
+            name=form.cleaned_data.get("name"),
+            area=form.cleaned_data.get("area"),
+            opening_hour=form.cleaned_data.get("opening_hour"),
+            closing_hour=form.cleaned_data.get("closing_hour"),
+            description=form.cleaned_data.get("description"),
+            contact=form.cleaned_data.get("contact"),
+            picture=form.cleaned_data.get("picture"),
+            tripadvisor_link=form.cleaned_data.get("tripadvisor_link"),
+        )
         return super(VenueCreationFormView, self).form_valid(form)
+
     def get_success_url(self):
-        return reverse('venuepage', kwargs={'pk': self.venue.pk})
-    
-    
+        return reverse("venuepage", kwargs={"pk": self.venue.pk})
+
+
 class TeamCreationFormView(FormView):
-    template_name = 'createteam.html'
+    template_name = "createteam.html"
     form_class = TeamCreationForm
-    success_url = '/'
+    success_url = "/"
+
     def form_valid(self, form):
         self.team = Team.objects.create(
-            name=form.cleaned_data.get('name'),
-            moderator=form.cleaned_data.get('moderator'),
-            )
-        for player in form.cleaned_data['team_user']:
-                self.team.team_user.add(player),
+            name=form.cleaned_data.get("name"),
+            moderator=form.cleaned_data.get("moderator"),
+        )
+        for player in form.cleaned_data["team_user"]:
+            self.team.team_user.add(player),
         return super(TeamCreationFormView, self).form_valid(form)
+
     def get_success_url(self):
-        return reverse('teampage', kwargs={'pk': self.team.pk})
+        return reverse("teampage", kwargs={"pk": self.team.pk})
 
 
 class JoinGameView(LoginRequiredMixin, View):
@@ -347,19 +352,17 @@ class JoinGameView(LoginRequiredMixin, View):
             ScoreBoard.objects.create(
                 game=game,
                 user=request.user,
-                )
+            )
             activities = game.activities.all()
             for activity in activities:
                 ActivityCheck.objects.create(
-                    game=game,
-                    activity=activity,
-                    user=request.user
+                    game=game, activity=activity, user=request.user
                 )
             messages.success(request, "You have successfully joined the game")
 
-        return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
-    
-    
+        return HttpResponseRedirect(request.META.get("HTTP_REFERER", "/"))
+
+
 class LeaveGameView(LoginRequiredMixin, View):
     def post(self, request, game_id):
         game = get_object_or_404(Game, id=game_id)
@@ -378,7 +381,4 @@ class LeaveGameView(LoginRequiredMixin, View):
             ).delete()
             messages.success(request, "You have successfully joined the game")
 
-        return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
-       
-
-    
+        return HttpResponseRedirect(request.META.get("HTTP_REFERER", "/"))
